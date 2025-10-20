f58d867bd56f4e2c67ae3ce1d8dfee32ec2cf553
LazyWiz609 <lazywiz609@gmail.com>
2025-10-20 17:26:11 +0530

    Added env for storing keys

--- backend/db_sqls/config.php
+++ backend/db_sqls/config.php
@@ -1,4 +1,13 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$razorpay_key = $_ENV['RAZORPAY_KEY_ID'] ?? getenv('RAZORPAY_KEY_ID');
+$razorpay_secret = $_ENV['RAZORPAY_KEY_SECRET'] ?? getenv('RAZORPAY_KEY_SECRET');
+
 $host = "localhost";
 $user = "root";          // your db username
 $pass = "";              // your db password
