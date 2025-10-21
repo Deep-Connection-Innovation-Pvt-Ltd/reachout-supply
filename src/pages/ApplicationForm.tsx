bd909ed602d6fc2c2c2ba0abec82b4b6551ca5c5
Krishna Dubey <krishnadubey@example.com>
2025-10-21 22:31:19 +0530

    updated code

--- src/pages/ApplicationForm.tsx
+++ src/pages/ApplicationForm.tsx
@@ -169,7 +169,7 @@ export default function ApplicationForm({ plan }: ApplicationFormProps) {
 
     const steps = useMemo(() => [
         {
-            title: "Personal Info",
+            title: "Personal Informations",
             description: "Basic details",
             path: "personal-details"
         },
